c7416d4ffea04a50f2b03cf2e00cd6cea7800552
Manuel Alejandro da Costa <manuelalejandrodacostacontreras>
2020-10-26 13:27:18 +0100

    feat: added toggle menu and contact functionality

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -29,11 +29,11 @@ const Header = () => {
             </Link>
           </li>
           <li>
-            <a className={styles.link}>Contáctame</a>
+            <a className={styles.link} onClick={toggleContactModal} >Contáctame</a>
           </li>
         </ul>
       </nav>
-      <div className={styles.trigger}>
+      <div className={styles.trigger} onClick={toggleMenu} >
         {menu && <Cross className={styles.cross} />}
         {!menu && <Menu className={styles.menu} />}
       </div>
